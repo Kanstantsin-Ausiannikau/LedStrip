1f8048e3242cf020f2bb81e90224ac7f940f8eef
Kanstantsin Ausiannikau <2000@tut.by>
2020-04-05 10:54:30 +0300

    Add ota, many other changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,46 @@
 #include <Arduino.h>
 #include "FastLED.h"
+#include "effects.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <EEPROM.h>
+
+#ifndef STASSID
+//#define STASSID "TP-Link_DB4C"
+#define STASSID "WifiNet4"
+#define STAPSK  "22349026"
+#endif
+#define EEPROM_SIZE 1
+
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
 // Подключаем библиотеку FastLED.
 
-typedef void (* GenericFP)(); //function pointer prototype to a function which takes an 'int' an returns 'void'
+typedef void (*GenericFP)(); //function pointer prototype to a function which takes an 'int' an returns 'void'
 
 void shift();
 void effect1();
 void effect2();
 void effect3();
+void effect4();
+void effect5();
 void keyPressed();
 void debounceTime();
-void rainbowCycle(int SpeedDelay);
-void setPixel(int Pixel, byte red, byte green, byte blue);
-void showStrip();
-byte * Wheel(byte WheelPos);
-void setPixelHeatColor (int Pixel, byte temperature);
-void Fire(int Cooling, int Sparking, int SpeedDelay);
-void BouncingBalls(byte red, byte green, byte blue, int BallCount);
-void setAll(byte red, byte green, byte blue);
 
-GenericFP effects[3] = {&effect1, &effect2, &effect3}; //create an array of 'GenericFP' function pointers. Notice the '&' operator
+
+GenericFP effects[5] = {&effect1, &effect2, &effect3, &effect4, &effect5}; //create an array of 'GenericFP' function pointers. Notice the '&' operator
 
 // Указываем, какое количество пикселей у нашей ленты.
 #define LED_COUNT 180
- 
+
 // Указываем, к какому порту подключен вход ленты DIN.
 #define LED_PIN 13
 #define BUTTON_PIN 12
- 
+
 // Создаем переменную strip для управления нашей лентой.
 CRGB strip[LED_COUNT];
 
@@ -37,13 +50,49 @@ int debounceTimeCounter;
 
 bool doBreaked;
 
-
 long startTimer;
 
- 
 void setup()
 {
   delay(3000);
+
+    Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");  //  "Начало OTA-апдейта"
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");  //  "Завершение OTA-апдейта" 
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    //  "Ошибка при аутентификации"
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    //  "Ошибка при начале OTA-апдейта"
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    //  "Ошибка при подключении"
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    //  "Ошибка при получении данных"
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    //  "Ошибка при завершении OTA-апдейта"
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");  //  "Готово"
+  Serial.print("IP address: ");  //  "IP-адрес: "
+  Serial.println(WiFi.localIP());
+  
+  //EEPROM.begin(EEPROM_SIZE);
 
   // Добавляем ленту
   FastLED.addLeds<WS2812B, LED_PIN, RGB>(strip, LED_COUNT);
@@ -52,241 +101,91 @@ void setup()
 
   buttonState = false;
 
-  currentEffectIndex = 0;
+  //currentEffectIndex = EEPROM.read(0);
+
+  //currentEffectIndex = 0;
   debounceTimeCounter = 0;
 
   doBreaked = false;
 
 
-  Serial.begin(115200);
 
   attachInterrupt(BUTTON_PIN, keyPressed, HIGH);
 }
 
- void ICACHE_RAM_ATTR keyPressed()
- {
-    timer1_isr_init();
-    timer1_attachInterrupt(debounceTime);
-    timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);
-    timer1_write(10000);
- }
+void ICACHE_RAM_ATTR keyPressed()
+{
+  timer1_isr_init();
+  timer1_attachInterrupt(debounceTime);
+  timer1_enable(TIM_DIV256, TIM_EDGE, TIM_LOOP);
+  timer1_write(10000);
+}
 
- void ICACHE_RAM_ATTR debounceTime()
- {
-   if (digitalRead(BUTTON_PIN)==HIGH)
-   {
-     Serial.println(millis() - startTimer);
-     Serial.println("pressed");
-     doBreaked = true;
-
-     timer1_disable();
-   }
- }
+void ICACHE_RAM_ATTR debounceTime()
+{
+  if (digitalRead(BUTTON_PIN) == HIGH)
+  {
+    Serial.println(millis() - startTimer);
+    doBreaked = true;
+    timer1_disable();
+    currentEffectIndex++;
+    //EEPROM.write(0, currentEffectIndex);
+    //EEPROM.commit();
+    //system_restart();
+  }
+}
 
 void loop()
 {
-   if (doBreaked)
-   {
-     currentEffectIndex++;
+  ArduinoOTA.handle();
 
-     if (currentEffectIndex==sizeof(effects) / sizeof(int))
-     {
-       currentEffectIndex = 0;
+  if (doBreaked)
+  {
+    //currentEffectIndex++;
+
+    if (currentEffectIndex == sizeof(effects) / sizeof(int))
+    {
+      currentEffectIndex = 0;
     }
+
     doBreaked = false;
-   }
-  
+  }
+
   effects[currentEffectIndex]();
 }
 
 void shift()
 {
-  for(int i=LED_COUNT;i>0;i--)
+  for (int i = LED_COUNT; i > 0; i--)
   {
-    strip[i] = strip[i-1];
+    strip[i] = strip[i - 1];
   }
 }
 
 void effect1()
 {
-  rainbowCycle(20);  
-}
-
-void rainbowCycle(int SpeedDelay) {
-  byte *c;
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< LED_COUNT; i++) {
-
-      if (doBreaked)
-      {
-        for (int i = 0; i < LED_COUNT; i++)
-        {
-          strip[i] = CRGB::Black; // Черный цвет, т.е. выключено.
-        }
-        // Передаем цвета ленте.
-        FastLED.show();
-        return;
-      }
-
-      c=Wheel(((i * 256 / LED_COUNT) + j) & 255);
-      setPixel(i, *c, *(c+1), *(c+2));
-    }
-    showStrip();
-    delay(SpeedDelay);
-  }
-}
-
-void setPixel(int Pixel, byte red, byte green, byte blue) 
-{
-   strip[Pixel].r = red;
-   strip[Pixel].g = green;
-   strip[Pixel].b = blue;
-}
-
-void setAll(byte red, byte green, byte blue) {
-  for(int i = 0; i < LED_COUNT; i++ ) {
-    setPixel(i, red, green, blue);
-  }
-  showStrip();
-}
-
-byte * Wheel(byte WheelPos) 
-{
-  static byte c[3];
- 
-  if(WheelPos < 85) {
-   c[0]=WheelPos * 3;
-   c[1]=255 - WheelPos * 3;
-   c[2]=0;
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   c[0]=255 - WheelPos * 3;
-   c[1]=0;
-   c[2]=WheelPos * 3;
-  } else {
-   WheelPos -= 170;
-   c[0]=0;
-   c[1]=WheelPos * 3;
-   c[2]=255 - WheelPos * 3;
-  }
-
-  return c;
-}
-
-void showStrip()
-{
-  FastLED.show();
+  rainbowCycle(strip, 20);
 }
 
 void effect2()
 {
-  Fire(55,120,15);
-}
-
-void Fire(int Cooling, int Sparking, int SpeedDelay) {
-  static byte heat[LED_COUNT];
-  int cooldown;
- 
-  // Step 1.  Cool down every cell a little
-  for( int i = 0; i < LED_COUNT; i++) {
-    cooldown = random(0, ((Cooling * 10) / LED_COUNT) + 2);
-   
-    if(cooldown>heat[i]) {
-      heat[i]=0;
-    } else {
-      heat[i]=heat[i]-cooldown;
-    }
-  }
- 
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for( int k= LED_COUNT - 1; k >= 2; k--) {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
-  }
-   
-  // Step 3.  Randomly ignite new 'sparks' near the bottom
-  if( random(255) < Sparking ) {
-    int y = random(7);
-    heat[y] = heat[y] + random(160,255);
-    //heat[y] = random(160,255);
-  }
-
-  // Step 4.  Convert heat to LED colors
-  for( int j = 0; j < LED_COUNT; j++) {
-    setPixelHeatColor(j, heat[j] );
-  }
-
-  showStrip();
-  delay(SpeedDelay);
-}
-
-void setPixelHeatColor (int Pixel, byte temperature) {
-  // Scale 'heat' down from 0-255 to 0-191
-  byte t192 = round((temperature/255.0)*191);
- 
-  // calculate ramp up from
-  byte heatramp = t192 & 0x3F; // 0..63
-  heatramp <<= 2; // scale up to 0..252
- 
-  // figure out which third of the spectrum we're in:
-  if( t192 > 0x80) {                     // hottest
-    setPixel(Pixel, 255, 255, heatramp);
-  } else if( t192 > 0x40 ) {             // middle
-    setPixel(Pixel, 255, heatramp, 0);
-  } else {                               // coolest
-    setPixel(Pixel, heatramp, 0, 0);
-  }
+  Fire(strip, 55, 120, 15);
 }
 
 void effect3()
 {
-  BouncingBalls(0xff,0,0, 3);
+  SnowSparkle(strip, 0x10, 0x10, 0x10, 50, random(50, 500));
 }
 
-void BouncingBalls(byte red, byte green, byte blue, int BallCount) {
-  float Gravity = -9.81;
-  int StartHeight = 1;
- 
-  float Height[BallCount];
-  float ImpactVelocityStart = sqrt( -2 * Gravity * StartHeight );
-  float ImpactVelocity[BallCount];
-  float TimeSinceLastBounce[BallCount];
-  int   Position[BallCount];
-  long  ClockTimeSinceLastBounce[BallCount];
-  float Dampening[BallCount];
- 
-  for (int i = 0 ; i < BallCount ; i++) {  
-    ClockTimeSinceLastBounce[i] = millis();
-    Height[i] = StartHeight;
-    Position[i] = 0;
-    ImpactVelocity[i] = ImpactVelocityStart;
-    TimeSinceLastBounce[i] = 0;
-    Dampening[i] = 0.90 - float(i)/pow(BallCount,2);
-  }
-
-  while (true) {
-    for (int i = 0 ; i < BallCount ; i++) {
-      TimeSinceLastBounce[i] =  millis() - ClockTimeSinceLastBounce[i];
-      Height[i] = 0.5 * Gravity * pow( TimeSinceLastBounce[i]/1000 , 2.0 ) + ImpactVelocity[i] * TimeSinceLastBounce[i]/1000;
- 
-      if ( Height[i] < 0 ) {                      
-        Height[i] = 0;
-        ImpactVelocity[i] = Dampening[i] * ImpactVelocity[i];
-        ClockTimeSinceLastBounce[i] = millis();
- 
-        if ( ImpactVelocity[i] < 0.01 ) {
-          ImpactVelocity[i] = ImpactVelocityStart;
-        }
-      }
-      Position[i] = round( Height[i] * (LED_COUNT - 1) / StartHeight);
-    }
- 
-    for (int i = 0 ; i < BallCount ; i++) {
-      setPixel(Position[i],red,green,blue);
-    }
-   
-    showStrip();
-    setAll(0,0,0);
-  }
+void effect4()
+{
+  setAll(strip, 0xff, 0xff, 0xff);
+  delay(500);
 }
+
+void effect5()
+{
+  setAll(strip, 0, 0, 0);
+
+}
+
